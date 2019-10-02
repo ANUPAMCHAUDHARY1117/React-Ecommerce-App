8e29b7b0a85ad75f2dd667ac8e81a0baeae2d316
Anupam Chaudhary <anupam1996@gmail.com>
2019-10-02 14:58:09 +0530

    COMMIT::ANUPAM::Finishing Up Section 4, setting up ecommerce project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Homepage from './homepage.component';
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
   return (
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import directory from '../../components/directory/directory.component';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />    
+    </div>
+);
+
+export default HomePage; 
